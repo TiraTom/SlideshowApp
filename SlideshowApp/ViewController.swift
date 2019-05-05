d2c65c79999f42732cae25b32c69bf18c7830997
tiratom <backspace373@gmail.com>
2019-05-05 19:21:17 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by masao on 2019/05/05.
+//  Copyright © 2019 TiraTom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
